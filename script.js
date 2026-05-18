8d2c23c31f6648cd645dd276aa49d674b31424dd
giulioseb99 <giuliosebba@hotmail.com>
2026-05-18 19:42:49 +0200

    Refactor ghost button event handler
    Refactor ghost button click event to use a separate function for checking the ghost pin.

--- script.js
+++ script.js
@@ -380,7 +380,7 @@ const captionObserver = new IntersectionObserver((entries) => {
 if (caption) captionObserver.observe(caption);
 
 
-document.getElementById("ghostBtn").addEventListener("click", () => {
+function checkGhostPin() {
   const val = document.getElementById("ghostPin").value;
 
   if (val === "????") {
@@ -388,4 +388,5 @@ document.getElementById("ghostBtn").addEventListener("click", () => {
   } else {
     document.getElementById("ghostError").innerText = "✕";
   }
+}
 });
